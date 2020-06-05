df281d2f92f8d3bc918dcc097c847ea014146d22
shivanikota <57177241+shivanikota@users.noreply.github.com>
2020-06-05 23:45:11 +0530

    geo location removed

--- script.js
+++ script.js
@@ -1,76 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var lnkAll  = document.querySelector("#lnkAll");
-    var lnkNear = document.querySelector("#lnkNear");
-    var lnkMap  = document.querySelector("#lnkMap");
-
-    var mapContainer = document.querySelector("#mapContainer");
-    var list = document.querySelector("#list ul");
-    var listTitle = document.querySelector("#listTitle");
-
-    lnkAll.addEventListener("click", function() {
-        var html = "";
-        for (var r of restaurants) {
-            html += "<li><h3>" + r.name + "</h3>";
-            html += "<p><address>" + r.address + "</address></p>";
-            html += "</li>";
-        }
-        list.innerHTML = html;
-        listTitle.innerHTML = "All Restaurants";
-    });
-    lnkNear.addEventListener("click", function() {
-        navigator.geolocation.getCurrentPosition(
-            function(location) {
-                var lat = location.coords.latitude;
-                var lon = location.coords.longitude;
-                      var table = document.getElementById("table");
-                      var row = table.insertRow(1);
-                        var cell1 = row.insertCell(0);
-                        var cell2 = row.insertCell(1);
-                        cell1.innerHTML = lat+" "+lon;
-                        cell2.innerHTML ="30";
-            },
-            function() {
-                alert("We can't locate you");
-            },
-            {
-                enableHighAccuracy: true,
-                maximumAge: 60000,
-                timeout: 5000
-            }
-        );
-    });
-    lnkMap.addEventListener("click", function() {
-    initMap();
-        if (mapContainer.className == "hidden") {
-            mapContainer.className = "";
-            lnkMap.innerHTML = "show map";      
-        } else {
-            mapContainer.className = "hidden";
-            lnkMap.innerHTML = "show map";            
-        }
-    });
-});
-
-var map;
-
-function initMap() {
-    var uluru = {lat: 37.751, lng:-97.822};
-    var table = document.getElementById("table");
-    var row = table.insertRow(1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    cell1.innerHTML = uluru.lat+" "+uluru.lng;
-    cell2.innerHTML ="30";
-    map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
-        center: uluru
-    });
-}
 
 function myFunction() {
     var x = document.getElementById("frm1");
     var id = "";
-    lnkMap.innerHTML = "Locate Me";
     id = x.elements[0].value;
     if(id=="1000")
     {
